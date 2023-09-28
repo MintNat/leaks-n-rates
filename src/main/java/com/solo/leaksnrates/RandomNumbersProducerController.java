3df0babd39d86fecd734867dfa3f37c4327507e7
Natalia Miatowicz <Natalia.Miatowicz@lseg.com>
2023-09-28 12:45:27 +0200

    Change Random, add Readme, add testplan

--- src/main/java/com/solo/leaksnrates/RandomNumbersProducerController.java
+++ src/main/java/com/solo/leaksnrates/RandomNumbersProducerController.java
@@ -8,13 +8,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.random.RandomGenerator;
+import java.util.Random;
 import java.util.stream.IntStream;
 
 @Log
 @RestController
 public class RandomNumbersProducerController {
-    private final RandomGenerator randomGenerator = RandomGenerator.getDefault();
+    private final Random randomGenerator = new Random();
     private static final String LOG_REQUEST = "User requested %d numbers. Response: ";
     @Autowired
     FaultyStorage storage;
